2ecfee5e89c8ed5ea2956a9182f413de77c6bf0c
whiteclothes <33566466+whiteclothes@users.noreply.github.com>
2017-11-28 08:11:16 +0800

    Update IndexController.java

--- src/main/java/com/xiaozl/initialwork1/web/controller/IndexController.java
+++ src/main/java/com/xiaozl/initialwork1/web/controller/IndexController.java
@@ -2,14 +2,21 @@ package com.xiaozl.initialwork1.web.controller;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.xiaozl.initialwork1.entity.User;
 import com.xiaozl.initialwork1.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author xiaozl
@@ -21,7 +28,10 @@ public class IndexController {
 
     @Resource
     private UserService userService;
-
+    private static List<User> userList;
+    public IndexController(){
+        this.userList=new ArrayList<User>();
+    }
     @RequestMapping(value = {"", "/", "login"}, method = RequestMethod.GET)
     public String toLogin(HttpServletRequest request)
     {
@@ -53,4 +63,45 @@ public class IndexController {
             return "login";
         }
     }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public ModelAndView showReguster(HttpServletRequest request,HttpServletResponse response){
+        ModelAndView mav=new ModelAndView("register");
+        mav.addObject("User",new User());
+        return mav;
+    }
+   /* public String toRegister(HttpServletRequest request) throws Exception {
+        //If session have attribute "user", jump to index page, else jump to login page.
+        if (request.getSession().getAttribute("user") != null) {
+            String password = request.getParameter("password");
+            String userName = request.getParameter("userNmae");
+            userService.addUser(userName, password);
+
+        }
+        return  "register";
+    }*/
+    @RequestMapping(value = {"", "/", "register"}, method = RequestMethod.POST)
+    public ModelAndView addUser(HttpServletRequest request,HttpServletResponse response, @ModelAttribute("user")User user) throws Exception {
+        userService.newUser(user);
+        return new ModelAndView("index","entity",user.getUserName());
+
+    }
+
+   /* public String register(@RequestParam("userName") String userName,
+
+                           @RequestParam("password") String password,HttpServletRequest request) throws Exception {
+
+      //    Map<Long, User> users = new ConcurrentHashMap<Long, User>();
+
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        //存储user信息
+        userService.checkLogin(user);
+        userList.add(user);
+        //跳转页面到登录页面
+        return "login";
+    }*/
+    /*    @RequestMapping(value = "", method = RequestMethod.POST)
+*/
 }
